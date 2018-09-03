137f119eb7c3a5598305439277f22338648d7a1e
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 13:03:31 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
-# code your #valid_move? method here
+# code your #valid_move? method Here
 
+def position_taken?(arr, index)
+  if arr[index] == "X" || arr[index] == "O"
+    return true
+  else arr[index] == " " || arr[index] == "" || arr[index] == nil
+    return false
+  end
+end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above
+
+def valid_move?(arr, index)
+  if !position_taken?(arr, index) && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end

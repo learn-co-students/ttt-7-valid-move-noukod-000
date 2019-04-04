0a802347bf47004fe5e7b2b389ef64915feb8a1f
Ricardo Paul <kingupper0@gmail.com>
2019-04-04 16:32:32 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
+# the valid_move method is a complete validation method
+# it ensures that the position is not taken and 
+# the index is comprised in the array
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      return true
+    end
+  else
+    return false
+  end
+end 
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return FALSE
+  else
+    return TRUE
+  end
+end 
